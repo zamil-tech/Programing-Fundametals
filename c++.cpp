4e1947d285d5e96f979e5defa490182c5d704df2
Muzamil <90958923+zamil623@users.noreply.github.com>
2021-12-03 07:18:52 -0500

    Add files via upload
    Print Hello world in c++

---
+++ c++.cpp
@@ -0,0 +1,20 @@
+#include<iostream>   // this iostream is a header file without this you cant run your program
+using namespace std;  /// this is also this helps in printing the data
+
+
+// here int is a data type and main is function that does mean main will return int data
+int main(){
+	// cout is used for printing the data 
+	cout<<"hello world ";
+	/// for cout std is important 
+	
+	
+	
+	
+	// we wrote return 0 because as earlier we said that this function will return int so 0 is a int
+	return 0;
+	
+}
+///  run program with f11 
+// you can see output 
+	
